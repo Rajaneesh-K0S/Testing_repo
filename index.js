c8318320bca70c3259d2cca76a9aa1d6edcbf932
Rajaneesh-K0S <108665914+Rajaneesh-K0S@users.noreply.github.com>
2022-07-05 10:14:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 const express = require("express");
 const app = express();
 
-app.get('/',(req,res,next)=>{res.send('hi')});
 
 app.listen(3000,()=>console.log("hello"));
 console.log("hello poi from github");
